582889a16b4279dbb2fcdef3c47ecf04a5cdb777
kranthiraj217 <69861789+kranthiraj217@users.noreply.github.com>
2020-09-06 14:45:50 +0530

    Delete speakGoodBye.js

--- mod4-solution/speakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function speak(w){
-var byeSpeaker ={};
-var speakword="Good Bye";
-byeSpeaker.sayBye = function() {
-	console.log(speakword + " " + byeSpeaker.name);
-}
-			window.byeSpeaker = byeSpeaker;
-})(window);
