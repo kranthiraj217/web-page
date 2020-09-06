40b16448067a977aa1eb8680da1409225859e9ac
kranthiraj217 <69861789+kranthiraj217@users.noreply.github.com>
2020-09-06 14:45:59 +0530

    Delete speakHello.js

--- mod4-solution/speakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window){
-var helloSpeaker = {};
-var speakWord="Hello ";
-helloSpeaker.sayHello = function() {
-	console.log(speakWord + " " + helloSpeaker.name);
-}
-            window.helloSpeaker = helloSpeaker;
-})(window);
